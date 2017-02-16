cbbd8b88031a445635537112393f3d454768bea8
Chris O'Brien <chris@superleague.com>
2017-02-15 22:23:11 -0800

    refactor

--- nginx/parse_logs.py
+++ nginx/parse_logs.py
@@ -2,32 +2,24 @@
 grouping requests by /24 subnet and day
 and then give me the top 10 subnets from each day'''
 
-from subprocess import call
 import re
 from collections import Counter
 
-for day in range(1, 32):
-    if len(str(day)) == 1:
-        search_day = "0" + str(day)
-    else:
-        search_day = str(day)
+for day in range(1,32):
+	if len(str(day)) == 1:
+		search_day = "0" + str(day)
+	else:
+		search_day = str(day)
 
-    search_day += "/Feb"
+	total_day = Counter()
 
-    print search_day
-    subnet_dict = Counter()
+	with open("test.log") as f:
+		for chunk in iter(lambda: f.read(1024 * 1024), ''):
+			match = re.findall(".*\[%s/Feb.*\" ([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+" % search_day, chunk)
+			total_day.update(Counter(match))
+	f.close()
 
-    with open("test.log") as f:
-        i=0
-        for chunk in iter( : f.read(1024 * 1024), ''):
-            match = re.findall(".*\[%s.*\" ([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+ [0-9\.]+" % search_day, chunk)
-            i += 1
-            #print "processing chunk %d" % i
-            if len(match) > 0:
-                subnet_dict_chunk = Counter(match)
+	print search_day
+	print total_day.most_common(10)
 
-                #print "got a match in chunk %d" % i
-                #print subnet_dict_chunk
-                subnet_dict.update(subnet_dict_chunk)
 
-    print subnet_dict.most_common(10)
