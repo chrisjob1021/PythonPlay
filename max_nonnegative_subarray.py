50e1d2f72e9d5ec1dbd719e265ccf89efaac6e64
Chris O'Brien <chris@superleague.com>
2017-04-13 15:28:19 -0700

    bug fixes

--- max_nonnegative_subarray.py
+++ max_nonnegative_subarray.py
@@ -23,47 +23,44 @@ class Solution:
     	if len(A) == 0:
     		return []
 
-    	def sub(start):
-    		if start > len(A)-1:
-    			return
+        def addSol(sol):
+            if len(sol) > 0:
+                solL.append(sol)
 
-    		sol = [ ]
-    		for i in xrange(start, len(A)):
-    			if A[i] >= 0:
-    				sol.append(A[i])
-    			else:
-    				break
+        solL = [ ]
+        sol = [ ]
+        for i in xrange(len(A)):
+            if A[i] >= 0:
+                sol.append(A[i])
+            else:
+                addSol(sol)
+                sol = [ ]
+                continue
 
-    		if len(sol) > 0:
-    			solL.append(sol)
+        addSol(sol)
 
-    		sub(start+len(sol)+1)
+        solD = {}
 
-    	solL = [ ]
+        for i in xrange(len(solL)):
+            sumSub = sum(solL[i])
+            if sumSub in solD:
+                solA, solB = solL[i], solD[sumSub]
+                if len(solA) > len(solB):
+                    solD[sumSub] = solL[i]
+                elif len(solA) == len(solB):
+                    if solA[0] < solB[0]:
+                        solD[sumSub] = solL[i]
+            else:
+                solD[sumSub] = solL[i]
 
-    	sub(0)
-
-    	solD = {}
-
-    	for i in xrange(len(solL)):
-    		sumSub = sum(solL[i])
-    		if sumSub in solD:
-    			solA, solB = solL[i], solL[solD[sumSub]]
-    			if len(solA) > len(solB):
-    				solD[sumSub] = solL[i]
-    			elif len(solA) == len(solB):
-    				if solA[0] < solB[0]:
-    					solD[sumSub] = solL[i]
-    		else:
-    			solD[sumSub] = solL[i]
-
-    	if len(solD) > 0:
-    		return solD[max(solD.keys())]
-    	else:
-    		return []
+        if len(solD) > 0:
+            return solD[max(solD.keys())]
+        else:
+            return []
 
 
 print Solution().maxset([1, 2, 5, -7, 2, 3])
 print Solution().maxset([-1, -1, -1, -7, -1, -1])
 print Solution().maxset([1, 2, 5])
 print Solution().maxset([756898537, -1973594324, -2038664370, -184803526, 1424268980])
+print Solution().maxset([ 0, 0, -1, 0 ])
